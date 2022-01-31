7d77f9a9a09e2a032e60edfbc90fa1430322e921
Gabrieldm14 <93100414+Gabrieldm14@users.noreply.github.com>
2022-01-31 00:00:20 +0000

    Add files via upload

---
+++ Ocho.php
@@ -0,0 +1,16 @@
+<html>
+    <head></head>
+
+<body>
+<p>Nº de jugadores en el equipo:</p>
+<?php
+   $x = 1;
+
+     do {
+         echo "Jugador: $x <br>";
+     $x++;
+     } while ($x <= 11);
+?> 
+
+</body>
+</html>
